960b52b3ff51b0b5a5ac302b969241ae8948e94a
Azat <azat.25.01@mail.ru>
2021-02-24 21:22:10 +0600

    Cho hochesh

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,99 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Adidas Womens Cloudfoam Pure Running Shoe',
+    price: 49,
+    description: 'Explore your surroundings. These adidas running-inspired shoes feature a foot-hugging knit upper and a female-friendly fit. Soft midsole cushioning adds comfort as you head out for coffee or discover a busy side street.',
+    rating: '4.8/5',
+    img: "https://m.media-amazon.com/images/I/81zc8i+8qfL._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/adidas-Performance-Womens-Cloudfoam-Running/dp/B0711R2TNB/ref=sr_1_2?dchild=1&keywords=sneakers&qid=1614105393&sr=8-2"
+  },
+  {
+    id: 2,
+    name: 'PUMA Womens Carina Sneaker',
+    price: 40,
+    description: 'Carina l takes inspiration from styles From the 80s - interpreted to fit todays laid-back sneaker look of californian beach towns. The leather upper is accented by a perforated vamp and a PUMA formstripe. The highlight of the shoe is the new cupsole tooling though with a slightly elevated platform sole with texture interest in toe and heel area and cat branding in the medial. The SoftFoam+ sockliner provides superior cushioning and optimal comfort for every step of your day.',
+    rating: '4.7/5',
+    img: "https://m.media-amazon.com/images/I/71FfObARk-L._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/PUMA-Womens-Carina-Sneaker-Silver/dp/B07HJLYCJT/ref=sr_1_3?dchild=1&keywords=sneakers&qid=1614105393&sr=8-3"
+  },
+  {
+    id: 3,
+    name: 'Under Armour Mens Charged Assert 8 Running Shoe',
+    price: 60,
+    description: 'Neutral: for runners who need a balance of flexibility & cushioning. Lightweight mesh upper with 3-color digital print delivers complete breathability. Durable leather overlays for stability & that locks in your midfoot. Eva sockliner provides soft, step-in comfort. Charged cushioning midsole uses compression molded foam for even greater responsiveness & durability, providing optimal cushioning & energy return. Solid rubber outsole covers high impact zones for greater durability with less weight. Offset:. weight: 9.9 oz.',
+    rating: '4.7/5',
+    img: "https://m.media-amazon.com/images/I/513DZ5zwCiL._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/Under-Armour-Charged-Assert-Running/dp/B07G7TDPS1/ref=sr_1_4?dchild=1&keywords=sneakers&qid=1614105393&sr=8-4"
+  },
+  {
+    id: 4,
+    name: 'Adidas Womens Grand Court Sneaker',
+    price: 45,
+    description: 'A 70s style reborn. These womens shoes take inspiration from iconic sport styles of the past and move them into the future. The shoes are made of a durable leather-like upper with signature 3-Stripes along the sides. Plush midsole cushioning gives comfort to every step.',
+    rating: '4.7/5',
+    img: "https://m.media-amazon.com/images/I/71qdoDlEOpL._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/adidas-Womens-Grand-Court-Black/dp/B07DBHRKL9/ref=sr_1_5?dchild=1&keywords=sneakers&qid=1614105393&sr=8-5"
+  },
+  {
+    id: 5,
+    name: 'Fila Womens Disruptor II Sneaker',
+    price: 47,
+    description: 'This lightweight, retro-inspired sneaker from Fila is perfect for every day and then some. Made with a durable nubuck upper with a smooth, soft finish, this shoe features a removable cushioned insole and an extra-thick rubber sole for excellent shock absorption. A padded tongue and collar and soft lining keep feet feeling fresh and comfortable throughout the day.',
+    rating: '4.6/5',
+    img: "https://m.media-amazon.com/images/I/71ANbA7wX+L._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/Fila-Womens-Disruptor-Premium-Sneakers/dp/B0743TXFSY/ref=sr_1_6?dchild=1&keywords=sneakers&qid=1614105393&sr=8-6"
+  },
+  {
+    id: 6,
+    name: 'Nike Womens Downshifter 9 Sneaker',
+    price: 61,
+    description: 'The Nike Downshifter 9 womens running shoes provides lightweight breathable comfort throughout your run. These womens sneakers have additional durability with a rubber outsole and closed mesh through the midfood and heel.',
+    rating: '4.6/5',
+    img: "https://m.media-amazon.com/images/I/81k46w6gjSL._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/Nike-Womens-Downshifter-Sneaker-Black/dp/B07H84QFHN/ref=sr_1_7?dchild=1&keywords=sneakers&qid=1614105393&sr=8-7"
+  },
+  {
+    id: 7,
+    name: 'Аdidas Mens Grand Court Sneaker',
+    price: 50,
+    description: 'A 70s style reborn. These shoes take inspiration from iconic sport styles of the past and move them into the future. The shoes craft an everyday look with a smooth leather upper. Signature 3-Stripes flash along the sides. Plush midsole cushioning gives comfort to every step.',
+    rating: '4.6/5',
+    img: "https://m.media-amazon.com/images/I/61FbqzPD8TL._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/adidas-Grand-Court-Black-White/dp/B07D9H183S/ref=sr_1_8?dchild=1&keywords=sneakers&qid=1614105393&sr=8-8"
+  },
+  {
+    id: 8,
+    name: 'Skechers Mens Classic Fit-Delson-Camden Sneaker',
+    price: 61,
+    description: 'Round toe mesh slip on with bungee cord lace type.Trendsetters looking for a kick that wins at both style and comfort will appreciate the New Balance FuelCore Nergize lifestyle shoe. Practical and stylish, these sneakers have a sleek, easy-to-wear bootie-style upper that hugs your foot to provide a snug and supportive fit while also adding a stylish finish to your look.',
+    rating: '4.6/5',
+    img: "https://images-na.ssl-images-amazon.com/images/I/71pqOp-2sfL._AC_UX575_.jpg",
+    url: "https://www.amazon.com/Skechers-Classic-Fit-Delson-Camden-Sneaker-Black/dp/B075686DJ5/ref=sr_1_9?dchild=1&keywords=sneakers&qid=1614105393&sr=8-9"
+  },
+  {
+    id: 9,
+    name: 'Skechers Mens Afterburn Memory-Foam Lace-up Sneaker',
+    price: 50,
+    description: 'Smooth leather, synthetic and mesh fabric upper in a sporty training sneaker with perforated details, stitching and overlay accents. Padded collar and tongue, fabric lining, Memory Foam insole, Articu-Lyte rubber sole and a flexible, high-traction rubber sole. 1 ½ " built-in heel.',
+    rating: '4.6/5',
+    img: "https://m.media-amazon.com/images/I/71+BksVBGTL._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/Skechers-AFTER-BURN-M-FIT-Memory-Lace-Up-Sneaker/dp/B00D881G1I/ref=sr_1_10?dchild=1&keywords=sneakers&qid=1614105393&sr=8-10"
+  },
+  {
+    id: 10,
+    name: 'New Balance Womens FuelCore Nergize V1 Sneaker',
+    price: 40,
+    description: 'Trendsetters looking for a kick that wins at both style and comfort will appreciate the New Balance FuelCore Nergize lifestyle shoe. Practical and stylish, these sneakers have a sleek, easy-to-wear bootie-style upper that hugs your foot to provide a snug and supportive fit while also adding a stylish finish to your look.',
+    rating: '4.5/5',
+    img: "https://m.media-amazon.com/images/I/71GyzXlZlRL._AC_UL480_FMwebp_QL65_.jpg",
+    url: "https://www.amazon.com/New-Balance-Nergize-FuelCore-Sneaker/dp/B07C2JYB6Z/ref=sr_1_11?dchild=1&keywords=sneakers&qid=1614105393&sr=8-11"
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
